da17d9cb22c32d88940aa477df0f50e12d604844
Eric Lineback <eric@interopion.com>
2018-01-11 16:03:30 -0700

    Remove debug stuff.

--- src/js/launch.js
+++ src/js/launch.js
@@ -8,33 +8,23 @@ var default_config = 'config.json';
 var config_path = 'config/' + default_config;
 
 jQuery.get(config_path, function(data) {
-    data.forEach(function(obj, index){
-        console.log(index);
-        for (var key in obj){
-            console.log(key, obj[key]);
-        }
-    });
 
     for(var i = 0; i < data.length; i++){
-        console.log("data cid: " + i + " is " + data[i].client_id);
-        console.log("data ruri: " + i + " is " + data[i].redirect_uri);
-        console.log("data scope: " + i + " is " + data[i].scope);
         if (data[i].fhir_service === iss || (data[i].provider !== undefined && iss.indexOf(data[i].provider) > -1)){
             client_id = data[i].client_id;
-            console.log("client_id: " + client_id);
             redirect_uri = data[i].redirect_uri;
-            console.log("redirect_uri: " + redirect_uri);
             break;
         }
     }
 
     // No launch parameter provided. This is a standalone launch.
-    console.log("launch scope: " + scopes);
-    console.log("launch : " + launch);
     if (launch === "") {
         scopes = scopes + " launch/patient";
     }
 
+    console.log("client_id: " + client_id);
+    console.log("scopes : " + scopes);
+    console.log("redirect_uri : " + redirect_uri);
     if (redirect_uri !== null) {
         FHIR.oauth2.authorize({
             "client_id": client_id,
